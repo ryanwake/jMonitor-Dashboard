3c28c4417438658d54d95f43966c48495418ce00
Ryan Wake <rwake345@gmail.com>
2016-11-29 20:15:46 -0500

    Added index.html

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.get('/login', function(req, res) {
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+    res.redirect('/index.html');
 });
 
 router.get('/logout', function(req, res) {
